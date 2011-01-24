d323dd8328e8d28826a6a8beb5809918ae07e1f0
ma86jian <ma86jian@f7dc2746-886a-e028-aa8a-8f8a8d456117>
2011-01-24 16:52:48 +0000

    added class Char

--- trunk/website.py
+++ trunk/website.py
@@ -16,6 +16,13 @@ g['GridN'] = GridN
 app = web.application(urls, globals())
 render = web.template.render('templates/', globals = g, base = 'base')
 
+
+class Char:
+	def __init__(self, num, val):
+		self.num = num
+		self.val = val
+
+
 class index:
 	"""shows all functions link"""
 	def GET(self):
@@ -27,32 +34,34 @@ class main:
 	def GET(self):
 		charsfile = open('./static/json/characters.json', 'r').read()
 		Chars = json.loads(charsfile, 'gbk')
-		NumChar = len(Chars)
+		CountChar = len(Chars)
 		
-		phrasesfile = open('./static/json/phrases.json', 'r').read()
-		Phrases = json.loads(phrasesfile, 'gbk')
-		NumPhrase = len(Phrases)
-		numShow = 3
+		wordsfile = open('./static/json/phrases.json', 'r').read()
+		Words = json.loads(wordsfile, 'gbk')
+		CountWord = len(Words)
+		CountShow = 3
 		
-		chnum = [[None] * GridN for i in range(GridN)]
-		chars = [[None] * GridN for i in range(GridN)]
-		phrases = [None] * numShow
-		used = [False] * NumPhrase
+		chargrid = [[None] * GridN for i in range(GridN)]
+		words = [None] * CountShow
+		used = [False] * CountWord
 		
-		for i in range(numShow):
-			selectNum = random.randrange(0, NumPhrase)
-			while used[selectNum]:
-				selectNum = random.randrange(0, NumPhrase)
+		for i in range(CountShow):
+			wordnum = random.randrange(0, CountWord)
+			while used[wordnum]:
+				wordnum = random.randrange(0, CountWord)
 			else:
-				used[selectNum] = True
-				phrases[i] = Phrases[selectNum]
+				used[wordnum] = True
+				words[i] = [None] * len(Words[wordnum])
+				for j, charval in enumerate(Words[wordnum]):
+					charnum = Chars.index(charval)
+					words[i][j] = Char(charnum, charval)
 		
 		for i in range(GridN):
 			for j in range(GridN):
-				chnum[i][j] = random.randrange(0, NumChar)
-				chars[i][j] = Chars[chnum[i][j]]
+				charnum = random.randrange(0, CountChar)
+				chargrid[i][j] = Char(charnum, Chars[charnum])
 		
-		return render.main(phrases, chars, chnum)
+		return render.main(words, chargrid)
 
 
 class setting:
